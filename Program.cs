d8e04c55ee3673c930817945dd9a5f0cc40c37e8
VDelarosa <vmdel@DESKTOP-QCM166L>
2025-02-04 14:02:38 -0600

    add project to repository add fundamentals exercises 1-4 add text output to assignment to check work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is Game 1377");
+        }
+    }
+}
